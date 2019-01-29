891846dc4adde6731ed90b3683de9959d8fb004b
tannerjt <biolsurfer85@gmail.com>
2019-01-29 11:46:52 -0800

    update naming backuparcgis-service

--- test/script.js
+++ test/script.js
@@ -1,9 +1,9 @@
-const BackupArcGIS = require('../index.js')
+const BackupArcGISService = require('../index.js')
 
 if(require.main == module) {
     const outDir = './require'
 
-    const Backup1 = new BackupArcGIS('https://services.arcgis.com/uUvqNMGPm7axC2dD/arcgis/rest/services/state_parks/FeatureServer/0', outDir)
+    const Backup1 = new BackupArcGISService('https://services.arcgis.com/uUvqNMGPm7axC2dD/arcgis/rest/services/state_parks/FeatureServer/0', outDir)
     Backup1.run().then((resp) => {
         if(!resp.duplicate) {
             console.log(`${resp.serviceDetails.name} completed: ${resp.filename}`)
@@ -12,7 +12,7 @@ if(require.main == module) {
         }
     })
  
-    const Backup2 = new BackupArcGIS('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/Brookings_Sites/FeatureServer/0', outDir)
+    const Backup2 = new BackupArcGISService('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/Brookings_Sites/FeatureServer/0', outDir)
     Backup2.run().then((resp) => {
         if(!resp.duplicate) {
             console.log(`${resp.serviceDetails.name} completed: ${resp.filename}`)
@@ -21,7 +21,7 @@ if(require.main == module) {
         }
     })
 
-    const Backup3 = new BackupArcGIS('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/Business_Oregon_Industrial_Sites/FeatureServer/0', outDir)
+    const Backup3 = new BackupArcGISService('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/Business_Oregon_Industrial_Sites/FeatureServer/0', outDir)
     Backup3.run().then((resp) => {
         if(!resp.duplicate) {
             console.log(`${resp.serviceDetails.name} completed: ${resp.filename}`)
@@ -30,7 +30,7 @@ if(require.main == module) {
         }
     })
 
-    const Backup4 = new BackupArcGIS('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/OREGON_EDUCATIONAL_BOUNDARIES/FeatureServer/0', outDir)
+    const Backup4 = new BackupArcGISService('https://services.arcgis.com/uUvqNMGPm7axC2dD/ArcGIS/rest/services/OREGON_EDUCATIONAL_BOUNDARIES/FeatureServer/0', outDir)
     Backup4.run().then((resp) => {
         if(!resp.duplicate) {
             console.log(`${resp.serviceDetails.name} completed: ${resp.filename}`)
